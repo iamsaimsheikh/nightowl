ce964f1a3455ce449b4b0d33f1295fd410780e69
iamsaimsheikh <75107444+iamsaimsheikh@users.noreply.github.com>
2021-03-19 04:28:07 +0500

    Third Commit

--- src/Components/Design.js
+++ src/Components/Design.js
@@ -1,12 +1,121 @@
-import React from 'react'
+import React,{Component, useEffect,useState} from 'react'
 import '../../src/App.css'
+import Ester from '../Content/Portraits/ester [Recovered]-01.jpg'
+import Junaid from '../Content/Portraits/junaid [Recovered]-01.jpg'
+import Qurat from '../Content/Portraits/qurat-final-01.jpg'
+import Tajammul from '../Content/Portraits/t-tajjamul-01-01.png'
+import Tanzeel from '../Content/Portraits/tanzeel-01.jpg'
+import Najeeba from '../Content/Portraits/Najeeba.jpg'
+import Snow from './Snow'
+
+
 
 function Design() {
 
+    
+    const [images,setImages] = useState(
+        [
+            {
+                id:0,
+                previous:Tanzeel,
+                current:Ester,
+                next:Junaid,
+            },
+            {
+                id:1,
+                previous:Ester,
+                current:Junaid,
+                next:Qurat,
+            },
+            {
+                id:2,
+                previous:Junaid,
+                current:Qurat,
+                next:Tajammul,
+            },
+            {
+                id:3,
+                previous:Qurat,
+                current:Tajammul,
+                next:Tanzeel,
+            },
+            {
+                id:4,
+                previous:Tajammul,
+                current:Tanzeel,
+                next:Najeeba,
+            },
+            {
+                id:5,
+                previous:Tanzeel,
+                current:Najeeba,
+                next:Ester,
+            },
+    
+        ]
+    
+    )
+
+    const [index,setIndex] = useState(2);
+    
+
+    
+
+
     return (
-        <div className="secondContainer">
+        <div className="secondContainer" onLoad={setTimeout(() => {
             
+        }, 2000)}>
+            
+            <div className='portraitContainer' id='portraitContainer' >
+                
+                <div className='img-div'>
+                    <img src={images[index].previous} />
+                </div>
+                <div className="logicSign" onClick={() => {
+
+                        if(index == images.length -1){
+                            setIndex(0);
+                        }
+                        else{
+                            setIndex(prevIndex => prevIndex + 1);
+                        }
+
+                        var portraitContainer = document.getElementById('portraitContainer')
+                            portraitContainer.replaceWith(portraitContainer)
+                                  
+                    }}>
+                &lt;
+                </div>
+                <div className='img-div-center' >
+                    <img src={images[index].current} id='img-div-center'  />
+                </div >
+                <div className='logicSign' onClick={() => { 
+                    
+                    if(index == 0){
+                        setIndex(4);
+                    }
+                    else{
+                        setIndex(prevIndex => prevIndex - 1);
+                    }
+
+                        var portraitContainer = document.getElementById('portraitContainer')
+                            portraitContainer.replaceWith(portraitContainer)       
+                    }}>
+                &gt;
+                </div>
+                <div className='img-div'>
+                    <img src={images[index].next}/>
+                </div>    
+                
+                
+
+                 </div>
+                 
+            
+                 
         </div>
+        
     )
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,48 +4,42 @@ import FLogo from '../Content/fiver-logo.png'
 import VLogo from '../Content/vecstacy-logo.png'
 import Landing from './Landing'
 import Design from './Design'
+import Snow from './Snow'
 
 
 function Home() {
     
     const [secondContainer,setsecondContainer] = useState('Home');
+    const [navDark,setNavDark] = useState(false)
 
-    const switchToDesign = (screen) => {
-       var container = document.getElementById('container');
-       container.className="containerDark";
-       var navLinks= document.getElementById('nav-links').getElementsByTagName('a');
-       navLinks[0].setAttribute("class","nav-links-dark")
-       navLinks[1].setAttribute("class","nav-links-dark")
-       navLinks[2].setAttribute("class","nav-links-dark")
+    const switchToDark = (screen) => {
+       setNavDark(true)
        setsecondContainer(screen)
     }
 
 
     return (
-        <div className='container' id='container'>
+        <div className={navDark? 'containerDark' : 'container'}>
+            <Snow color={navDark===false? '#666666' : '#f7f7f7'}/>
             <div className='nav-bar'>
                 <div className='logo' onClick={()=>{
-                    document.getElementById('container').className="container"
-                    var navLinks= document.getElementById('nav-links').getElementsByTagName('a');
-                    navLinks[0].removeAttribute("class");
-                    navLinks[1].removeAttribute("class");
-                    navLinks[2].removeAttribute("class");
+                    setNavDark(false)
                     setsecondContainer('Home');
                     }}></div>
-                    <div className='nav-links' id="nav-links"> 
-                        <a  href='#' onClick={() => switchToDesign('Design')}>Design</a>
-                        <a  href='#' onClick={switchToDesign}>Wordpress</a>
-                        <a  href='#' onClick={switchToDesign}>MERN</a>
+                    <div className={navDark ? 'nav-links-dark':'nav-links'}> 
+                        <a   href='#' onClick={() => switchToDark('Design')}>Design</a>
+                        <a   href='#' onClick={switchToDark}>Wordpress</a>
+                        <a   href='#' onClick={switchToDark}>MERN</a>
                     </div>
                 </div>
                 {/* Paste After This */}
                 {secondContainer ==='Home'? <Landing/> : <Design />}
                         
             {/* Paste Before This */}
-            <div className='contact'>+923485516445   |   iamsaimsheikh@gmail.com   | 
-            <img src={FLogo} height={17} width={17}/> 
-             <a href='https://www.fiverr.com/iamsaimsheikh?' > Fiverr</a> |
-             <img src={VLogo} height={15} width={17}/> 
+            <div className={navDark ? 'contact-dark':'contact'}>+923485516445    |        iamsaimsheikh@gmail.com      |    
+              <img src={FLogo} height={17} width={17}/>   
+             <a href='https://www.fiverr.com/iamsaimsheikh?' >   Fiverr</a>   |
+               <img src={VLogo} height={15} width={17}/> 
              <a href='https://www.instagram.com/thevecstacy/' > The Vecstacy</a>
              </div>
         </div>
